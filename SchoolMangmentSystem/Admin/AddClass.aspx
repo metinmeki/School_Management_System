c8acfa9787db53422f36676454ab46dcd6c67637
metin_meki <123455084+metinmeki@users.noreply.github.com>
2025-07-15 15:56:14 +0300

    Updated and developed AddClass.aspx and other pages

--- SchoolMangmentSystem/Admin/AddClass.aspx
+++ SchoolMangmentSystem/Admin/AddClass.aspx
@@ -9,18 +9,42 @@
                 <asp:Label ID="lblMsg" runat="server"></asp:Label>
             </div>
             <h3 class="text-center">Class Name </h3>
-            <div class="row mb-3 mr-lg-5 ml-lg-5 mt--md-5">
-                <div class="col-md-6"> 
+                <div class="row mb-3 mr-lg-5 ml-lg-5 mt--md-5">
+                    <div class="col-md-6"> 
                     <label for="txtClass">Class Name</label>
-                    <asp:TextBox ID="txtClass" runat="server" CssClass="form-control" placeholder="Enter Class Name" required></asp:TextBox>
+                    <asp:TextBox ID="txtClass" runat="server" CssClass="form-control" placeholder="Enter Class Name" required Width="1448px" Height="22px"></asp:TextBox>
                 </div>
             </div>
 
             <div class="row mb-3 mr-lg-5 ml-lg-5">
                 <div class="col-md-3 col-md-offset-2 mb-3">
-                    <asp:Button ID="btnAdd" runat="server" CssClass="btn btn-primary btn-block" BackColor="#5558C9" Text="Add Class" OnClick="btnAdd_Click1" Height="29px"/>
+                    <asp:Button ID="btnAdd" runat="server" CssClass="btn btn-primary btn-block" BackColor="#5558C9" Text="Add Class" OnClick="btnAdd_Click1" Height="62px" Width="1454px"/>
                 </div> 
             </div>
+            <div class="row mb-3 mr-lg-5 ml-lg-5 mt--md-5">
+    <div class="col-md-6">
+        <asp:GridView ID="GridView1" runat="server" CssClass="table table-hover table-border" DataKeyNames="ClassId" AutoGenerateColumns="False"
+            EmptyDataText="No Record To Display !" OnPageIndexChanging="GridView1_PageIndexChanging" OnRowCancelingEdit="GridView1_RowCancelingEdit" 
+            OnRowEditing="GridView1_RowEditing" OnRowUpdating="GridView1_RowUpdating" AllowPagin="True" PageSize="4" Width="1380px" Height="178px">
+            <Columns>
+                <asp:BoundField DataField="Sr.NO" HeaderText="Sr.NO" ReadOnly="True">
+                <ItemStyle HorizontalAlign="Center" />
+                </asp:BoundField>
+                <asp:TemplateField HeaderText="Class">
+                    <EditItemTemplate>
+                        <asp:TextBox ID="txtClassEdit" runat="server" Text='<%# Eval("ClassName") %>' CssClass="form-control"></asp:TextBox>
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:Label ID="lblClassName" runat="server" Text='<%# Eval("ClassName") %>'></asp:Label>
+                    </ItemTemplate>
+                    <ItemStyle HorizontalAlign="Center" />
+                </asp:TemplateField>
+                <asp:CommandField CausesValidation="False" HeaderText="Operation" />
+            </Columns>
+            <HeaderStyle BackColor="#5558C9" ForeColor="White"/>
+        </asp:GridView>
+    </div>
+                </div>
         </div>
     </div>
 
@@ -30,6 +54,8 @@
 
 <asp:Content ID="Content4" runat="server" contentplaceholderid="ContentPlaceHolder2">
     <style type="text/css">
+        .table-border {}
+        .form-control {}
         .btn-block {}
     </style>
 </asp:Content>
--- SchoolMangmentSystem/Admin/AddClass.aspx.cs
+++ SchoolMangmentSystem/Admin/AddClass.aspx.cs
@@ -1,17 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using static SchoolMangmentSystem.Models.CommanFn;
 
 namespace SchoolMangmentSystem.Admin
 {
     public partial class AddClass : System.Web.UI.Page
     {
+        Commanfnx fn = new Commanfnx();
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if(!IsPostBack)
+            {
+                GetClass();
+            }
         }
 
         protected void btnAdd_Click(object sender, EventArgs e)
@@ -19,9 +25,74 @@ namespace SchoolMangmentSystem.Admin
 
         }
 
+        private void GetClass()
+        {
+            DataTable dt = fn.fetch("Select Row_NUMBER() over(Order by (Select 1)) as [Sr.No],ClassId,ClassName from Class");
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
         protected void btnAdd_Click1(object sender, EventArgs e)
         {
+            try
+            {
+                DataTable dt = fn.fetch("Select * from Class where ClassName ='"+txtClass.Text.Trim()+"' ");
+                if(dt.Rows.Count == 0)
+                {
+                    string query = "Insert into Class values('"+txtClass.Text.Trim()+ "')";
+                    fn.Query(query);
+                    lblMsg.Text = "Inserted succesffully!";
+                    lblMsg.CssClass = "alert alert-success";
+                    txtClass.Text = String.Empty;
+                    GetClass();
+                }
+                else
+                {
+                    lblMsg.Text = "Inserted Class Already Exists!";
+                    lblMsg.CssClass = "alert alert-danger";
+                }
 
+            }
+            catch (Exception ex )
+            {
+                Response.Write("<script>alert(''"+ex.Message+"'')</script>");
+            }
+        }
+
+        protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridView1.PageIndex = e.NewPageIndex;
+            GetClass();
+        }
+
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex = -1;
+            GetClass();
+
+        }
+
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+        }
+
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            try
+            {
+                GridViewRow row = GridView1.Rows[e.RowIndex];
+                int cId =  Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
+                string ClassName = (row.FindControl("txtClassName") as TextBox).Text;
+                fn.Query("Update Class Set ClassName = '" + ClassName + "' where ClassId = '" + cId + "'");
+                lblMsg.Text = "Class Upload succesffully!";
+                lblMsg.CssClass = "alert alert-success";
+                GridView1.EditIndex= -1;
+                GetClass();
+            }
+            catch(Exception ex)
+            {
+                Response.Write("<script>alert(''" + ex.Message + "'')</script>");
+            }
         }
     }
 }
