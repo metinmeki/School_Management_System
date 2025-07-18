34d2d8694e99d5831bfddeafbdc27c2113ce0b3f
metin_meki <123455084+metinmeki@users.noreply.github.com>
2025-07-18 08:24:17 +0300

    Created Subject Form With UI

---
+++ SchoolMangmentSystem/Admin/Subject.aspx
@@ -0,0 +1,81 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin/AdminMst.Master" AutoEventWireup="true" CodeBehind="Subject.aspx.cs" Inherits="SchoolMangmentSystem.Admin.Subject" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder2" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+        <div style="background-image:url('../Images/'); width:100%; height:720px; background-repeat:no-repeat; background-size:cover; background-attachment:fixed;">
+        <div class="container p-md-4 p-sm-4">
+            <div>
+                <asp:Label ID="lblMsg" runat="server"></asp:Label>
+            </div>
+
+            <h3 class="text-center">New  Subject</h3>
+
+            <div class="row mb-3 mr-lg-5 ml-lg-5 mt-3">
+                <div class="col-md-6"> 
+                    <label for="ddlClass">Class</label>
+                    <asp:DropDownList ID="ddlClass" runat="server" CssClass="form-control">
+                        <asp:ListItem Text="Select Class" Value="" />
+                    </asp:DropDownList>
+                    <asp:RequiredFieldValidator 
+                        runat="server" 
+                        ErrorMessage="Class Is Required" 
+                        ControlToValidate="ddlClass" 
+                        Display="Dynamic" 
+                        ForeColor="Red" 
+                        InitialValue="Select Class" 
+                        SetFocusOnError="True" />
+                </div>
+
+                <div class="col-md-6"> 
+                    <label for="txtSubject">Subject</label>
+                    <asp:TextBox ID="txtSubject" runat="server" CssClass="form-control" placeholder="Enter Subject"  Width="100%" Height="22px" required></asp:TextBox>
+                </div>
+            </div>
+
+            <div class="row mb-3 mr-lg-5 ml-lg-5">
+                <div class="col-md-3 col-md-offset-2 mb-3">
+                    <asp:Button ID="btnAdd" runat="server" CssClass="btn btn-primary btn-block" BackColor="#5558C9" Text="Add Subject" OnClick="btnAdd_Click" Width="1530px" />
+                </div> 
+            </div>
+
+
+            <div class="row mb-3 mr-lg-5 ml-lg-5 mt-3">
+                <div class="col-md-12">
+<asp:GridView 
+    ID="GridView1" 
+    runat="server"
+    EmptyDataText=" NO Record To Display !"
+    CssClass="table table-hover table-bordered"
+    AutoGenerateColumns="False"
+    Width="100%" AllowPagin="True" PageSize="4" OnPageIndexChanging="GridView1_PageIndexChanging" DataKeyNames="SubjectId"
+    OnRowCancelingEdit="GridView1_RowCancelingEdit" OnRowEditing="GridView1_RowEditing"
+    OnRowUpdated="GridView1_RowUpdated" OnRowUpdating="GridView1_RowUpdating" >
+    <Columns>
+        <asp:BoundField DataField="Sr.NO" HeaderText="Sr.No">
+        <ItemStyle HorizontalAlign="Center" />
+        </asp:BoundField>
+        <asp:BoundField DataField="ClassName" HeaderText="Class" ReadOnly="True">
+        <ItemStyle BorderStyle="None" HorizontalAlign="Center" />
+        </asp:BoundField>
+        <asp:TemplateField HeaderText="Subejct">
+            <EditItemTemplate>
+                <asp:TextBox ID="TextBox1" runat="server" Text='<%# Eval("SubjectName") %>' CssClass="form-control"></asp:TextBox>
+            </EditItemTemplate>
+            <ItemTemplate>
+                <asp:Label ID="Label1" runat="server" Text='<%# Eval("SubjectName") %>'></asp:Label>
+            </ItemTemplate>
+            <ItemStyle HorizontalAlign="Center" />
+        </asp:TemplateField>
+<asp:CommandField HeaderText="Operation" CausesValidation="false" ShowEditButton="True">
+        <ItemStyle HorizontalAlign="Center" />
+        </asp:CommandField>
+    </Columns>
+     <HeaderStyle BackColor="#5558C9" ForeColor="White"/>
+                    </asp:GridView>
+                </div>
+            </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ SchoolMangmentSystem/Admin/Subject.aspx.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using static SchoolMangmentSystem.Models.CommanFn;
+
+namespace SchoolMangmentSystem.Admin
+{
+    public partial class Subject : System.Web.UI.Page
+    {
+        Commanfnx fn = new Commanfnx();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GetClass();
+                GetSubject();
+            }
+        }
+
+        private void GetClass()
+        {
+            DataTable dt = fn.fetch("SELECT * FROM Class");
+            ddlClass.DataSource = dt;
+            ddlClass.DataTextField = "ClassName";
+            ddlClass.DataValueField = "ClassId";
+            ddlClass.DataBind();
+            ddlClass.Items.Insert(0, "Select Class");
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string classVal = ddlClass.SelectedItem.Text;
+
+                DataTable dt = fn.fetch("SELECT * FROM Subject WHERE ClassId = '" + ddlClass.SelectedItem.Value + 
+                                        "', SubjectName='"+txtSubject.Text.Trim()+"'");
+                if (dt.Rows.Count == 0)
+                {
+                    string query = "INSERT Into SubjectId  VALUES ('" + ddlClass.SelectedItem.Value + "', '" + txtSubject.Text.Trim() + "')";
+                    fn.Query(query);
+
+                    lblMsg.Text = "Inserted Successfully!";
+                    lblMsg.CssClass = "alert alert-success";
+
+                    ddlClass.SelectedIndex = 0;
+                    txtSubject.Text = String.Empty;
+                    GetSubject();
+                }
+                else
+                {
+                    lblMsg.Text = "Entered Subject Already Exist for <b>" + classVal + "</b>!";
+                    lblMsg.CssClass = "alert alert-danger";
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "')</script>");
+            }
+        }
+
+        private void GetSubject()
+        {
+            DataTable dt = fn.fetch(@"SELECT Row_NUMBER() OVER (ORDER BY (SELECT 1)) AS [Sr.NO],
+                              s.SubjectId, s.ClassId, c.ClassName,
+                              FROM Subject s INNER JOIN Class c ON c.ClassId = s.ClassId");
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+
+        protected void btnAdd_Click1(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridView1.PageIndex = e.NewPageIndex;
+            GetSubject();
+        }
+
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex = -1;
+            GetSubject();
+        }
+
+       
+
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+            GetSubject();
+        }
+
+        protected void GridView1_RowUpdated(object sender, GridViewUpdatedEventArgs e)
+        {
+
+        }
+
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            try
+            {
+                GridViewRow row = GridView1.Rows[e.RowIndex];
+                int feeId = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
+                string feesAmt = (row.FindControl("TextBox1") as TextBox).Text;
+                fn.Query("Update Fees Set FeesAmount = '" + feesAmt.Trim() + "' where FeesId = '" + feeId + "' ");
+                lblMsg.Text = "Fees Updated Successfully!";
+                lblMsg.CssClass = "alert alert-success";
+                GridView1.EditIndex = -1;
+                GetSubject();
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert(''" + ex.Message + "'')</script>");
+            }
+        }
+    }
+}
