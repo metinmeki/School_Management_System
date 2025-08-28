17ec19808ac3035e23606dd36d5900a205620c04
metin_meki <123455084+metinmeki@users.noreply.github.com>
2025-08-29 02:53:49 +0300

    Teacher Subject Section design Done

--- SchoolMangmentSystem/Admin/TeachersSubject.aspx.cs
+++ SchoolMangmentSystem/Admin/TeachersSubject.aspx.cs
@@ -1,23 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using static SchoolMangmentSystem.Models.CommanFn;
 
 namespace SchoolMangmentSystem.Admin
 {
     public partial class TeachersSubject : System.Web.UI.Page
     {
+
+
+
+        Commanfnx fn = new Commanfnx();
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
+        private void GetClass()
+        {
+            DataTable dt = fn.fetch("SELECT * FROM Class");
+            ddlSubject.DataSource = dt;
+            ddlSubject.DataTextField = "ClassName";
+            ddlSubject.DataValueField = "ClassId";
+            ddlSubject.DataBind();
+            ddlSubject.Items.Insert(0, "Select Class");
+        }
+
+        private void GetTeacher()
+        {
+            DataTable dt = fn.fetch("SELECT * FROM Teacher");
+            ddlTeacher.DataSource = dt;
+            ddlTeacher.DataTextField = "Name";
+            ddlTeacher.DataValueField = "TeacherId";
+            ddlTeacher.DataBind();
+            ddlTeacher.Items.Insert(0, "Select Teacher");
+        }
 
         protected void ddlClass_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            string classId = ddlClass.SelectedValue;
+            DataTable dt = fn.fetch("SELECT * FROM Subject where ClassId='"+ classId +"'");
+            ddlSubject.DataSource = dt;
+            ddlSubject.DataTextField = "ClassName";
+            ddlSubject.DataValueField = "ClassId";
+            ddlSubject.DataBind();
+            ddlSubject.Items.Insert(0, "Select Class");
         }
+
+
 
         protected void btnAdd_Click(object sender, EventArgs e)
         {
